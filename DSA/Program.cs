660e6804f9670d948df47c0d5e0430ea906ffaac
Sudame, Paritosh <Paritosh.Sudame@wolterskluwer.com>
2024-04-02 13:03:26 +0530

    changes related to arrays medium

--- DSA/Program.cs
+++ DSA/Program.cs
@@ -153,5 +153,5 @@
 
 //PrefixSumQuestions.setMatrixZero();
 
-int[] nums = new int[] { 100, 4, 200, 1, 3, 2 };
+int[] nums = new int[] { 0, 3, 7, 2, 5, 8, 4, 6, 0, 1 };
 var ans = DSA.ArraysMedium.LongestConsecutive(nums);
