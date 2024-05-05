64b9dbe6e9d5ed75df04926341fe7adbf9cd1680
admin <admin@DESKTOP-QIOSETD>
2024-05-06 00:05:15 +0530

    changes related to peak elemetns

--- DSASheet/Revisor/BinarySearch.cs
+++ DSASheet/Revisor/BinarySearch.cs
@@ -387,5 +387,39 @@ namespace DSA
 
         }
 
+        /// <summary>
+        /// https://leetcode.com/problems/find-peak-element/
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int FindPeakElement(int[] nums)
+        {
+
+            int n = nums.Length;
+            int low = 1;
+            int high = n - 2;
+            int mid;
+
+            if (nums.Length == 1 || nums[0] > nums[1]) return 0;
+
+            if (nums[n - 1] >= nums[n - 2]) return n - 1;
+
+            while (low <= high)
+            {
+                mid = low + (high - low) / 2;
+
+                if (nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1])
+                    return mid;
+
+                if (nums[mid - 1] < nums[mid])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+
+            return -1;
+
+        }
+
     }
 }
