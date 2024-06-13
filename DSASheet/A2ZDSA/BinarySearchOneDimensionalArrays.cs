e5110c8e6149f91fd5ae2bc35a05dc86e962b85e
admin <admin@DESKTOP-QIOSETD>
2024-06-13 22:33:37 +0530

    changes realted to searchElementArray

--- DSASheet/A2ZDSA/BinarySearchOneDimensionalArrays.cs
+++ DSASheet/A2ZDSA/BinarySearchOneDimensionalArrays.cs
@@ -165,8 +165,47 @@ namespace DSA
 
             return firstElementIndex;
         }
+        //5, 6, 7, 8, 9, 10, 1, 2, 3
+        public static int searchElementRoatedArray(int[] nums, int target) {
 
+            int low = 0;
+            int high = nums.Length - 1;
+            int mid;
 
+            while(low <= high)
+            {
+                mid = low + (high - low) / 2;
+
+                if (nums[mid] == target)
+                    return mid;
+
+                if (nums[low] <= nums[mid])
+                {
+                    if (nums[low] <= target && target <= nums[mid])
+                    {
+                        high = mid - 1;
+                    }
+                    else
+                    {
+                        low = mid + 1;
+                    }
+                }
+                else
+                {
+                    if (nums[mid] <= target && target <= nums[high])
+                    {
+                        low = mid + 1;
+                    }
+                    else
+                    {
+                        high = mid - 1;
+                    }
+
+                }
+            }
+
+            return -1;
+        }
 
     }
 }
