19a67bc62f4ddc8cc75174353f7395b7d413ee6d
admin <admin@DESKTOP-QIOSETD>
2024-06-18 09:37:39 +0530

    Binary Search

--- DSASheet/A2ZDSA/BinarySearchOneDimensionalArrays.cs
+++ DSASheet/A2ZDSA/BinarySearchOneDimensionalArrays.cs
@@ -234,5 +234,35 @@ namespace DSA
             return ans;
         }
 
+        public static int FindPeak(int[] nums)
+        {
+            int n = nums.Length;
+            int low = 1;
+
+            int high = n - 2;
+
+            if (n == 1 || nums[0] > nums[1]) return 0;
+
+            if (nums[n - 1] > nums[n - 2]) return n - 1;
+            int mid;
+
+            while (low <= high)
+            {
+
+                mid = low + (high - low) / 2;
+
+                if (nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1])
+                    return mid;
+
+                if (nums[mid] > nums[mid + 1])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+
+            }
+
+            return -1;
+        }
+
     }
 }
